931d4241d8b747ea1532317ddc92e58c773f8d70
naomimori1 <naomi.mori@usp.br>
2025-03-10 17:37:15 -0300

    save current testeInContext.py version

--- testeInContext.py
+++ testeInContext.py
@@ -6,7 +6,7 @@ import sys
 
 modelfile = '''
 FROM llama2:7b
-PARAMETER temperature 0.01
+PARAMETER temperature 0.02
 SYSTEM "You are a helpful assistant who can only reply with numbers from 1 to 5, and nothing else."
 '''
 
