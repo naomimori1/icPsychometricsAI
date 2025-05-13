5f7aba88c1ff07a39682c50ed9f4dc80ffebf834
naomimori1 <naomi.mori@usp.br>
2025-05-13 19:09:27 -0300

    Analise total de Psycho50

--- ufsAnalysis.r
+++ ufsAnalysis.r
@@ -1,23 +1,27 @@
 # Load required libraries
+
 library(psych)
-library(ufs) 
+library(ufs)
 library(ggplot2)
 library(gridExtra)
 library(nortest)  # For Anderson-Darling test
 
-#' This code performs an analysis on a dataframe corresponding to one of the domains of the BFI-2 personality inventory, passed as an argument. 
-#' It begins by analyzing the structure of the dataframe using functions from the `ufs` package. 
-#' Subsequently, it computes the Mega analysis using the `psych` package directly. 
-#' A normality assessment is then conducted using the `ufs` package. 
-#' Throughout the program, relevant files and plots are generated and saved for further use or visualization.
-#' 
-#' @param filename The name of the CSV file containing the data (without the .csv extension).
+#' This code performs an analysis on a dataframe corresponding to one of the
+#' domains of the BFI-2 personality inventory, passed as an argument.  # nolint
+#' It begins by analyzing the structure of the dataframe using functions from the `ufs` package.  # nolint
+#' Subsequently, it computes the Mega analysis using the `psych` package directly.  # nolint
+#' A normality assessment is then conducted using the `ufs` package.
+#' Throughout the program, relevant files and plots are generated
+#'  and saved for further use or visualization.
+#'
+#' @param filename The name of the CSV file containing the data
+#' (without the .csv extension).
 #' @return None
 #' @examples
 #' # To run the analysis, use the command line:
 #' Rscript ufsAnalysis.r Extraversion
 #' #' # This will read the file "Extraversion.csv" and perform the analysis.
-#' 
+#'
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
@@ -36,15 +40,16 @@ item_scores <- data[, -1]
 cat("\n\n=== Scale Structure Analysis ===\n")
 results <- scaleStructure(
   data = item_scores,
-  digits = 3,         
-  ci = TRUE,          
+  digits = 3,
+  ci = TRUE,
   conf.level = 0.95,   # 95% confidence intervals
   omega.psych = TRUE,  # Include omega from psych package
   poly = TRUE          # Compute ordinal reliability estimates
 )
 
 # Save results to a text file and print to console
-sink(paste0(tools::file_path_sans_ext(basename(filename)), "_Reliability_Results.txt"), split = TRUE)
+sink(paste0(tools::file_path_sans_ext(basename(filename)),
+            "_Reliability_Results.txt"), split = TRUE)
 print(results)
 sink() # Properly close the sink
 
@@ -56,28 +61,33 @@ diagnosis <- scaleDiagnosis(
 )
 
 # Save the results to a text file and print to console
-sink(paste0(tools::file_path_sans_ext(basename(filename)), "_diagnosis.txt"), split = TRUE)
+sink(paste0(tools::file_path_sans_ext(basename(filename)),
+            "_diagnosis.txt"), split = TRUE)
 print(diagnosis)
 sink() # Properly close the sink
 
 # Print confirmation
 cat("\nAnalysis complete. Results saved to:\n")
-cat("- ", paste0(tools::file_path_sans_ext(basename(filename)), "_diagnosis.txt"), " (text summary)\n")
-cat("- ", paste0(tools::file_path_sans_ext(basename(filename)), "_scattermatrix.png"), " (scatter matrix plot)\n")
+cat("- ", paste0(tools::file_path_sans_ext(basename(filename)),
+                 "_diagnosis.txt"), " (text summary)\n")
+cat("- ", paste0(tools::file_path_sans_ext(basename(filename)),
+                 "_scattermatrix.png"), " (scatter matrix plot)\n")
 
 cat("\n\n=== Omega Assessment using psych package directly ===\n")
 resultado_omega <- omega(item_scores, nfactors = 1, plot = TRUE)
 
-#Print the omega results to console
+#  Print the omega results to console
 print(resultado_omega)
 
 # Save the omega results to a text file
-sink(paste0(tools::file_path_sans_ext(basename(filename)), "_omega_results.txt"))
+sink(paste0(tools::file_path_sans_ext(basename(filename)),
+            "_omega_results.txt"))
 print(resultado_omega)
 sink()
 
 # Save the omega plot
-png(paste0(tools::file_path_sans_ext(basename(filename)), "_omega_plot.png"), width = 800, height = 600)
+png(paste0(tools::file_path_sans_ext(basename(filename)),
+           "_omega_plot.png"), width = 800, height = 600)
 omega(item_scores, nfactors = 1, plot = TRUE)
 dev.off()
 
@@ -88,7 +98,9 @@ cat("\n\n=== Normality Assessment for ALL Participants Combined ===\n")
 result_all <- normalityAssessment(all_scores, samples = 10000, digits = 3)
 
 #Save normality plots
-png(paste0(tools::file_path_sans_ext(basename(filename)), "_normality_plots.png"), width = 10, height = 8, units = "in", res = 300)
+png(paste0(tools::file_path_sans_ext(basename(filename)),
+           "_normality_plots.png"), width = 10, height = 8, units = "in",
+    res = 300)
 grid.arrange(
   result_all$plot.sampleDist,
   result_all$plot.samplingDist,
@@ -102,6 +114,7 @@ dev.off()
 print(result_all)  # Isso mostrará estatísticas + plots no console
 
 # Se quiser salvar também em arquivo (opcional)
-sink(paste0(tools::file_path_sans_ext(basename(filename)), "_NormalityResults.txt"))
+sink(paste0(tools::file_path_sans_ext(basename(filename)),
+            "_NormalityResults.txt"))
 print(result_all)
 sink()
